fa76d6996f315761fda8e3f02f395cd86827cb18
Audriddax <audriddax113@gmail.com>
2024-10-26 02:35:25 -0400

    Maybe fix?

--- js/daxbot/components/background.js
+++ js/daxbot/components/background.js
@@ -9,7 +9,7 @@ const Background = () => {
     case "strippin":
       return html`
         <link rel="stylesheet" href="css/daxbot/strippin.css" />
-        <div class="row position-absolute vh-100 w-100 top-0">
+        <div class="background row position-absolute vh-100 w-100 top-0">
           <div class="col-9">
             <div class="d-flex justify-content-center align-items-center" style="height: 85%;">
               <div class="ratio ratio-1x1 rounded-circle logo" style="width: 40%;">
@@ -23,13 +23,13 @@ const Background = () => {
     case "kyle":
       return html`
         <link rel="stylesheet" href="css/daxbot/kyle.css" />
-        <video id="background" class="min-vh-100 min-vw-100 position-fixed top-0 left-0" autoplay muted loop>
+        <video class="background min-vh-100 min-vw-100 position-fixed top-0 left-0" autoplay muted loop>
           <source src="/Kyle_Wallpaper_Animation.mp4" type="video/mp4" />
         </video>
       `;
     default:
       return html`
-        <div class="position-absolute background vw-100 vh-100 d-flex align-items-center justify-content-center">
+        <div class="background position-absolute background vw-100 vh-100 d-flex align-items-center justify-content-center">
           <div class="loading"></div>
         </div>
       `;
