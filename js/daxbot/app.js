ec2274ff79138b7bc51a272aac6cae31e9134a6b
Audriddax <audriddax113@gmail.com>
2024-10-25 20:14:11 -0400

    Added strippin logo

--- js/daxbot/app.js
+++ js/daxbot/app.js
@@ -11,6 +11,15 @@ const App = () => {
 
   return html`
     <div class="container" style="max-width: 100%;">
+      <div class="row position-absolute vh-100 w-100 top-0">
+        <div class="col-9">
+          <div class="d-flex justify-content-center align-items-center" style="height: 85%;">
+            <div class="ratio ratio-1x1 rounded-circle logo" style="width: 40%;">
+              <img class="rounded-circle" src="strippin.png" />
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="row">
         <div class="col-9 vh-100 player" style="display: flex; justify-content: center;">
           <${VideoPlayer} />
