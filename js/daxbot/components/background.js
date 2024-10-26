0b427cb1d3d4a690dee4f045d6d4ec1a6cbee7f1
Audriddax <audriddax113@gmail.com>
2024-10-26 02:17:36 -0400

    Maybe fix?

--- js/daxbot/components/background.js
+++ js/daxbot/components/background.js
@@ -8,6 +8,7 @@ const Background = () => {
   switch (login) {
     case "strippin":
       return html`
+        <link rel="stylesheet" href="css/daxbot/strippin.css" />
         <div class="row position-absolute vh-100 w-100 top-0">
           <div class="col-9">
             <div class="d-flex justify-content-center align-items-center" style="height: 85%;">
@@ -21,6 +22,7 @@ const Background = () => {
     case "audriddax":
     case "kyle":
       return html`
+        <link rel="stylesheet" href="css/daxbot/kyle.css" />
         <video id="background" class="min-vh-100 min-vw-100 position-fixed top-0 left-0" autoplay muted loop>
           <source src="/Kyle_Wallpaper_Animation.mp4" type="video/mp4" />
         </video>
